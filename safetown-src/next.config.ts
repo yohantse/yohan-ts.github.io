6470f8bf14d585d1d833a4976d66e7e6ba8f9e2e
yohantse <yohantse121@gmail.com>
2026-06-25 13:57:43 +0300

    reorganize repo structure and update deployment to serve at /safetown

--- safetown-src/next.config.ts
+++ safetown-src/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/safetown-built",
+  basePath: "/safetown",
   images: {
     unoptimized: true,
   },
