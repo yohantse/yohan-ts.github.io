d19df1676d3ace7d4259b541fba2edb30db35056
yohantse <yohantse121@gmail.com>
2026-06-26 13:48:14 +0300

    Map integration.

--- safetown-src/src/components/Contact.tsx
+++ safetown-src/src/components/Contact.tsx
@@ -60,7 +60,28 @@ export default function Contact() {
               <div className={styles.cardContent}>
                 <h4>Office Location</h4>
                 <p>On The way from signal to megenagna<br />Zed bldg 3rd floor, Addis Ababa, Ethiopia</p>
+                <a
+                  href="https://maps.app.goo.gl/zRuTfQVnGgHTkGBT7"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.mapLink}
+                >
+                  View on Google Maps ↗
+                </a>
               </div>
+            </div>
+            <div className={styles.mapEmbed}>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3940.5!2d38.787804!3d9.021819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOcKwMDEnMTguNiJOIDM4wrA0NycxNi4xIkU!5e0!3m2!1sen!2set!4v1700000000000!5m2!1sen!2set"
+                width="100%"
+                height="220"
+                style={{ border: 0 }}
+                allowFullScreen={false}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Safe Town Electromechanical Office Location"
+                className={styles.mapIframe}
+              ></iframe>
             </div>
 
             {/* Phones */}
