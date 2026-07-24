69f4ac3afd106c0f45b91dae4623d44849ffc61e
Yohannes Tsegaye <yohantse121@gmail.com>
2026-07-24 14:50:16 +0300

    Update script.js

--- becca/script.js
+++ becca/script.js
@@ -1,15 +1,50 @@
-const buttons = document.querySelectorAll("button");
+function scrollToJournal(){
 
-buttons.forEach(button => {
+document
+.querySelector("#journal")
+.scrollIntoView({
+behavior:"smooth"
+});
 
-    button.addEventListener("click", () => {
+}
 
-        document
-        .querySelector("#journal")
-        .scrollIntoView({
-            behavior:"smooth"
-        });
 
-    });
+
+const observer =
+new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("active");
+
+}
 
 });
+
+});
+
+
+document
+.querySelectorAll(".reveal")
+.forEach(el=>observer.observe(el));
+
+
+
+
+// floating particles
+
+const particles=document.querySelector(".particles");
+
+
+for(let i=0;i<40;i++){
+
+let p=document.createElement("span");
+
+p.style.left=Math.random()*100+"%";
+p.style.animationDelay=Math.random()*10+"s";
+
+particles.appendChild(p);
+
+}
